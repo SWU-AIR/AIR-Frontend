829a34620d52f6a18ed735f68e30eb7e13830d53
xeangit <mily072@naver.com>
2022-11-28 16:06:54 +0900

    style: Modify CategoryScreen button style

--- air-app/screens/CategoryScreen.js
+++ air-app/screens/CategoryScreen.js
@@ -1,22 +1,28 @@
 import React from "react";
-import { StyleSheet, View, Text, Button } from "react-native";
+import { StyleSheet, View, Text, Button, Pressable } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 
 export default function CategoryScreen({navigation}) {
   return (
     <View style={styles.block}>
-      <Button
-        title="감정카드"
-        onPress={()=> navigation.navigate("EmotionList")}
-       />
-      <Button
-        title="퀴즈 맞추기"
-        onPress={()=> navigation.navigate("Quiz")}
-       />
-      <Button
-        title="감정 판단 AI"
-        onPress={()=> navigation.navigate("EmotionAI")}
-       />               
+      <View style={styles.buttonflex}>
+      <Pressable style={styles.button} onPress={()=> navigation.navigate("EmotionList")}>
+        <Text style={styles.text}>감정 카드</Text>
+        <Text style={styles.subtext}>감정 카드를 통해 기쁨, 슬픔, 놀람 등 다양한 감정들을 학습해보세요.</Text>
+      </Pressable>
+      </View>
+      <View style={styles.buttonflex}>
+      <Pressable style={styles.button} onPress={()=> navigation.navigate("Quiz")}>
+        <Text style={styles.text}>퀴즈 맞추기</Text>
+        <Text style={styles.subtext}>퀴즈를 통해 감정을 얼마나 이해할 수 있는지 확인해보세요.</Text>        
+      </Pressable>
+      </View>
+      <View style={styles.buttonflex}>
+      <Pressable style={styles.button} onPress={()=> navigation.navigate("EmotionAI")}>
+        <Text style={styles.text}>감정 판단 AI</Text>
+        <Text style={styles.subtext}>직접 사진을 찍어보고 사진 속 사람의 감정을 파악해보세요.</Text>
+      </Pressable>     
+      </View>         
     </View>
   );
 }
@@ -24,5 +30,40 @@ export default function CategoryScreen({navigation}) {
 const styles = StyleSheet.create({
   block: {
     flex: 1,
+    backgroundColor:'#FAFDFC'
   },
+  buttonflex:{
+    flex:0.3,
+  },
+  button:{
+    top:50,
+    left:50,
+    alignItems:'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 35,
+    borderRadius: 20,
+    width:'80%',
+    height:'80%',
+    elevation: 3,
+    backgroundColor: 'white',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+  },
+  text:{
+    fontSize: 20,
+    lineHeight: 40,
+    fontWeight: 'bold',
+    color: 'black',  
+  },
+  subtext:{
+    fontSize: 16,
+    lineHeight: 40,
+    color: 'black',  
+  }
 });
